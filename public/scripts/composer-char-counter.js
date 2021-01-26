f84cc5e2f718fe15e58ef8d8e873fc5d3b0f62b4
HaopengSun <sunhaopeng@gmail.com>
2021-01-26 19:20:30 +0000

    change shadow mouseover

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -8,8 +8,8 @@ $(document).ready(function() {
     }
   });
   $("article").mouseover(function() {
-    $(this).css("box-shadow", "5px 5px 5px #555");
-  }).mouseleave(function(){
+    $(this).css("box-shadow", "5px 5px 5px #555");})
+    .mouseleave(function(){
    $(this).css("box-shadow", "0px 0px 0px #555");
   });
 });
