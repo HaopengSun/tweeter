299f744f1fba40c00678ba06e8f1c6005ebd11c0
HaopengSun <sunhaopeng@gmail.com>
2021-01-26 18:33:48 +0000

    add shadow when mouseover

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-  console.log('composer char counter');
   $('textarea').on('input', function(){
     const textLength = $(this).val().length;
     const textLeft = 140 - textLength;
@@ -7,5 +6,10 @@ $(document).ready(function() {
     if (textLeft < 0){
       $('.counter').css('color', 'red');
     }
+  });
+  $("article").mouseover(function() {
+    $(this).css("box-shadow", "5px 5px 5px #555");
+  }).mouseleave(function(){
+   $(this).css("box-shadow", "0px 0px 0px #555");
   });
 });
