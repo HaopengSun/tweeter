8c7bddb664421bab360feb506cd63f54acc6ce06
HaopengSun <sunhaopeng@gmail.com>
2021-01-27 03:53:26 +0000

    detch tweets with ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,30 +7,12 @@
 // Fake data taken from initial-tweets.json
 
 $(document).ready(() => {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+
+  const url = '/tweets';
+  $.get(url, { method: 'GET' }).then(function(data){
+    const $tweet = renderTweets(data);
+    $('#tweet').append($tweet);
+  }).catch((err) => console.log(err));
 
   const renderTweets = function(tweets) {
     let result = '';
@@ -63,8 +45,31 @@ $(document).ready(() => {
     return $tweet;
   }
 
-  const $tweet = renderTweets(data);
-
-  // console.log($tweet);
-  $('#tweet').append($tweet);
+  $('.new-tweet').submit(function(event){
+    event.preventDefault();
+    const inputUser = $('#tweet-text').val();
+    const userName = $('#user-name').text();
+    const userPortrait = $('#user-portrait').attr('src');
+    const date = new Date();
+    const input =   {
+      "user": {
+        "name": userName,
+        "avatars": userPortrait,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": inputUser
+      },
+      "created_at": date
+    };
+    console.log(input);
+    const url = $('.new-tweet form').attr('action');
+    $.ajax({
+      url: ,
+      type: 'POST',
+      data: input
+    }).done(function(data){
+      $('#tweet').append(data);
+    });
+  })
 })
